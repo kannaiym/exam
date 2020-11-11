45ae5e8d58e5b93567682354f450ba8efe25e0e6
kannaiym <isheiym@gmail.com>
2020-11-11 23:37:40 +0600

    exam

---
+++ app/src/main/java/com/example/exam/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.exam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var textArray = mutableListOf<String>()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+            addArray()
+            removeArray()
+    }
+
+    private fun addArray() {
+        add_button.setOnClickListener click@ {
+            val value: String = add_edit_text.text.toString()
+              add_edit_text.text.clear()
+            Toast.makeText(, "Элемент не найден ", Toast.LENGTH_SHORT).show()
+            textArray.add(value)
+            displayArray()
+        }
+    }
+
+    private fun removeArray () {
+        remove_button.setOnClickListener {
+            val value: String = remove_edit_text.text.toString()
+            remove_edit_text.text.clear()
+            Toast.makeText(, "Невозможно добавить пустое знаечение", Toast.LENGTH_SHORT).show().
+            textArray.remove(value)
+            displayArray()
+        }
+    }
+
+}
+
